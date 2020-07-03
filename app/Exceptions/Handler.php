34bfd6301fb522b52099330e8b85443661a719b2
Mango <mangoxiaoda@gmail.com>
2020-07-03 07:19:54 +0000

    异常处理练习

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -14,6 +14,7 @@ class Handler extends ExceptionHandler
      */
     protected $dontReport = [
         //
+        InvalidRequestException::class,
     ];
 
     /**
@@ -24,6 +25,7 @@ class Handler extends ExceptionHandler
     protected $dontFlash = [
         'password',
         'password_confirmation',
+        
     ];
 
     /**
